770bca22eed7fde5fb031964b2779cd405b68b72
zib1995 <zib1995@mail.ru>
2022-10-12 08:56:54 +0300

    Added task netlist_reader

---
+++ task_netlist_reader/internal_format_example.py
@@ -0,0 +1,35 @@
+
+
+nets = [
+    {'type': 'P', 'left': 9, 'gate': 4, 'right': 10},
+    {'type': 'P', 'left': 0, 'gate': 1, 'right': 9},
+    {'type': 'P', 'left': 0, 'gate': 2, 'right': 9},
+    {'type': 'P', 'left': 0, 'gate': 3, 'right': 9},
+    {'type': 'P', 'left': 0, 'gate': 1, 'right': 11},
+    {'type': 'P', 'left': 11, 'gate': 2, 'right': 12},
+    {'type': 'P', 'left': 12, 'gate': 3, 'right': 10},
+    {'type': 'P', 'left': 0, 'gate': 4, 'right': 5},
+    {'type': 'P', 'left': 0, 'gate': 10, 'right': 6},
+    {'type': 'N', 'left': 15, 'gate': 4, 'right': 10},
+    {'type': 'N', 'left': 18, 'gate': 1, 'right': 15},
+    {'type': 'N', 'left': 18, 'gate': 2, 'right': 15},
+    {'type': 'N', 'left': 18, 'gate': 3, 'right': 15},
+    {'type': 'N', 'left': 18, 'gate': 1, 'right': 16},
+    {'type': 'N', 'left': 16, 'gate': 2, 'right': 17},
+    {'type': 'N', 'left': 17, 'gate': 3, 'right': 10},
+    {'type': 'N', 'left': 18, 'gate': 4, 'right': 5},
+    {'type': 'N', 'left': 18, 'gate': 10, 'right': 6}
+]
+#
+
+contacts = {
+    0: 'GND',
+    1: 'E',
+    2: 'X',
+    3: 'Y',
+    4: 'C',
+    5: 'S',
+    6: 'P'
+}
+#
+
